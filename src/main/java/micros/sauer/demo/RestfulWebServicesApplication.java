4b2a9317a40490c4781c0625ed4e989832ca9e56
Assaf Sauer <sauera@sauera-a02.vmware.com>
2021-11-08 15:50:53 +0100

    Initial commit

---
+++ src/main/java/micros/sauer/demo/RestfulWebServicesApplication.java
@@ -0,0 +1,21 @@
+package micros.sauer.demo;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServicesApplication.class, args);
+    }
+
+//	@Bean
+//	public LocaleResolver localeResolver() {
+//		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+//		localeResolver.setDefaultLocale(Locale.US);
+//		return localeResolver;
+//	}
+}
